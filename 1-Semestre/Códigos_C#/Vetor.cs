d666b0ea6363d9b343d60f75ed7d891e003145b9
Matheus Vilas Boas <110608299+MatVilasBoas@users.noreply.github.com>
2023-09-06 09:11:48 -0300

    Add files via upload

---
+++ 1-Semestre/Códigos_C#/Vetor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor
+{
+    public class Vetor
+    {
+        static void Main(string[] args)
+        {
+            int [] v = {2, 4, 6, 8, 10};
+
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine(v[i]);
+            }
+        }
+    }     
+}            
