dad0f21371a7416f7ecc8a62b584450c63e6e1b0
Matheus Vilas Boas <110608299+MatVilasBoas@users.noreply.github.com>
2023-09-25 19:19:09 -0300

    Update Contador.sql

--- 3-Semestre/SQL_Server/Contador.sql
+++ 3-Semestre/SQL_Server/Contador.sql
@@ -1,26 +1,27 @@
-create procedure contagem (@limite int, @tipo int)
-as begin
-   declare @i int
-
-   if @tipo = 1 -- regressiva
-      begin
-      set @i = @limite
-      while @i >= 1 begin
-         select valor = @i
-	     set @i = @i - 1
-      end
-   end
-
-   if @tipo = 2 -- progresiva
-      begin
-      set @i = 1
-      while @i <= @limite begin
-        select valor = @i
-	    set @i = @i + 1
-      end
-   end
-end
+ALTER PROCEDURE Contagem (@limite int, @tipo int)
+AS BEGIN
+	DECLARE @i int
+	if @tipo = 1 -- Regressiva
+	BEGIN
+		SET @i = @limite
+		while @i >= 1
+		BEGIN
+			SELECT Valor = @i
+			SET @i = @i - 1
+		END
+	END
+	if @tipo = 2 -- Crescente
+	BEGIN
+		SET @i = 1
+		while @i <= @limite
+		BEGIN
+			SELECT Valor = @i
+			SET @i = @i + 1
+		END
+	END
+END
 
 -----------------------------------------------------
 
 exec contagem 10, 1
+-- limite , tipo
